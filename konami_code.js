b3f63cf435b9592502a01acc0a2f31da07cc39fc
Ryan Patel <rajanpatel1011@gmail.com>
2018-03-27 23:38:44 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,21 +3,18 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 function init() {
   // Write your JavaScript code inside the init() function
 
- const body = document.querySelector('body')
+const body = document.querySelector('body');
     body.addEventListener('keydown',function(e){
-      alert(e.which);
-    });
-  
-
-/*
-  
+      
+      var key=[];
   for(let i=0;i<code.length;i++){
   key[i] = parseInt(e.detail||e.which);
   if(key[i]===code[i]){
     alert("Hurry!");
   }
   else{
-    e.prevaentDefault();
+    e.preventDefault();
   }
-}*/
+  }
+    });
 }
