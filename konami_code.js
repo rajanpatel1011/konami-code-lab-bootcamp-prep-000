8285a0b47038428d09461d91149ea9a8da9626da
Ryan Patel <rajanpatel1011@gmail.com>
2018-03-27 23:10:12 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,23 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
   // Write your JavaScript code inside the init() function
 
+
+document.addEventListener('keydown',function(e){
+ alert(event.which);
+  
+});
+
+
+  
+  for(let i=0;i<code.length;i++){
+  key[i] = parseInt(e.detail||e.which);
+  if(key[i]===code[i]){
+    alert("Hurry!");
+  }
+  else{
+    e.prevaentDefault();
+  }
+}
 }
